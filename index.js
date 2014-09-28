833ade1e7e25d2af5e4928fa6611883f02d7f8c5
James Hugman <james@hugman.tv>
2014-09-29 00:59:48 +0100

    Load in options from environment variables.
    Uses $OVERIDIFY_PREFIX as a prefix to a config key in the rules.js file.

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
 "use strict";
-var _ = require('underscore'),
+var path = require('path'),
+    _ = require('lodash'),
     transformTools = require('browserify-transform-tools'),
     fileFinder = require('./lib/file-selection'),
     defaultConfig = require('./overidify-rules'),
-    options = {
-      platform: 'android',
-      flavor: 'dev'
-    };
+    options;
+
+function findOptions (config) {
+  if (options) {
+    return options;
+  }
+  options = {};
+  var prefix = process.env.OVERIDIFY_PREFIX || '';
+  _.each(config, function (i, key) {
+    var value = process.env[prefix + key.toUpperCase()];
+
+    if (value) {
+      options[key] = value;
+
+    }
+  });
+  console.log('Using: ' + JSON.stringify(options));
+  return options;
+}
+
+
+// 1. Look in package.json for overidify object. 
+// 2. If it's a string, use it as a filename to a module.
+// 3. If it's an object, use that.
+// 4. If it's absent, use defaults.
 
 var transform = transformTools.makeRequireTransform("overidify",
-  {
-    jsFilesOnly: true, 
+  { 
     evaluateArguments: true
   },
   function(args, opts, cb) {
-    var requiredFile = args[0];
+    var requiredFile = args[0],
+        config = opts.config || defaultConfig;
     if (requiredFile[0] === '.') {
         var file;
         try {
-          file = fileFinder(opts.file, requiredFile, options, defaultConfig);
+          file = fileFinder(opts.file, requiredFile, findOptions(config), config);
           if (file !== requiredFile) {
             console.log('Swapping ' + requiredFile + ' for ' + file);
           }
