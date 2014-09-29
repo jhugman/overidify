f3fe7440c71f2bc2f1ecd3e84f60f93a97a1b923
James Hugman <james@hugman.tv>
2014-09-29 16:45:17 +0100

    Vaguely sensible defaults taken from the cordova website.

--- overidify-rules.js
+++ overidify-rules.js
@@ -3,8 +3,12 @@ module.exports = {
     'ios': ['ios', 'webview', 'webkit'],
     'android': ['android', 'webview', 'blink', 'webkit'],
     'fxos': ['fxos', 'nativejs', 'gecko'],
-    'desktop-fx': [],
-    'desktop-chrome': []
+    'fennec': ['fxos', 'nativejs', 'gecko'],
+    'blackberry': ['blackberry', 'webview'],
+    'blackberry10': ['blackberry10', 'webview'],
+    'wp8': ['wp8', 'windows', 'ie', 'webview'],
+    'wp7': ['wp7', 'windows', 'ie', 'webview'],
+    'tizen': ['tizen', 'webview']
   },  
   flavor: ['dev', 'debug', 'test', 'stage', 'production' ]
 };
